38ad4bf0b554b82861a69a2444d52aba2c71980c
Nils Van Geele <nils.vgeele@gmail.com>
2014-03-04 01:08:22 +0100

    Schema fix: lower case table names

--- documents/DB Design/xiast_schema.sql
+++ documents/DB Design/xiast_schema.sql
@@ -1,83 +1,37 @@
-# Dump of table Course
+# Dump of table Room
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `Course`;
+DROP TABLE IF EXISTS `room`;
 
-CREATE TABLE `Course` (
-  `course-code` varchar(32) NOT NULL DEFAULT '',
-  `title` varchar(126) DEFAULT NULL,
-  `description` text,
-  `titular-id` varchar(12) DEFAULT NULL,
-  `department` int(11) unsigned NOT NULL,
-  `grade` int(11) DEFAULT NULL,
-  PRIMARY KEY (`course-code`),
-  KEY `titular-id` (`titular-id`),
-  KEY `department` (`department`),
-  CONSTRAINT `course_ibfk_2` FOREIGN KEY (`department`) REFERENCES `Department` (`id`),
-  CONSTRAINT `course_ibfk_1` FOREIGN KEY (`titular-id`) REFERENCES `Person` (`netid`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table CourseActivity
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `CourseActivity`;
-
-CREATE TABLE `CourseActivity` (
+CREATE TABLE `room` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `course-code` varchar(32) NOT NULL DEFAULT '',
-  `type` int(11) unsigned NOT NULL,
-  `semester` int(11) unsigned NOT NULL,
-  `date` int(11) DEFAULT NULL,
-  `contact-time-hours` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `course-code` (`course-code`),
-  CONSTRAINT `courseactivity_ibfk_1` FOREIGN KEY (`course-code`) REFERENCES `Course` (`course-code`) ON DELETE CASCADE ON UPDATE CASCADE
+  `capacity` int(11) unsigned NOT NULL,
+  `building` varchar(5) NOT NULL DEFAULT '',
+  `floor` int(11) unsigned NOT NULL,
+  `number` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
-# Dump of table CourseEnrollment
+# Dump of table roomfacility
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `CourseEnrollment`;
+DROP TABLE IF EXISTS `roomfacility`;
 
-CREATE TABLE `CourseEnrollment` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `course-code` varchar(32) DEFAULT NULL,
-  `netid` varchar(12) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `course-code` (`course-code`),
-  KEY `netid` (`netid`),
-  CONSTRAINT `courseenrollment_ibfk_2` FOREIGN KEY (`netid`) REFERENCES `Person` (`netid`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `courseenrollment_ibfk_1` FOREIGN KEY (`course-code`) REFERENCES `Course` (`course-code`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `roomfacility` (
+  `room` int(11) unsigned NOT NULL,
+  `facility` int(11) unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
-# Dump of table CourseInstructor
+# Dump of table department
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `CourseInstructor`;
+DROP TABLE IF EXISTS `department`;
 
-CREATE TABLE `CourseInstructor` (
-  `course-activity` int(11) unsigned DEFAULT NULL,
-  `netid` varchar(12) DEFAULT NULL,
-  KEY `netid` (`netid`,`course-activity`),
-  KEY `course-activity` (`course-activity`),
-  CONSTRAINT `courseinstructor_ibfk_2` FOREIGN KEY (`netid`) REFERENCES `Person` (`netid`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `courseinstructor_ibfk_1` FOREIGN KEY (`course-activity`) REFERENCES `CourseActivity` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table Department
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `Department`;
-
-CREATE TABLE `Department` (
+CREATE TABLE `department` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `faculty` varchar(64) DEFAULT NULL,
   `name` varchar(64) DEFAULT NULL,
@@ -86,12 +40,12 @@ CREATE TABLE `Department` (
 
 
 
-# Dump of table Person
+# Dump of table person
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `Person`;
+DROP TABLE IF EXISTS `person`;
 
-CREATE TABLE `Person` (
+CREATE TABLE `person` (
   `netid` varchar(12) NOT NULL DEFAULT '',
   `firstname` varchar(64) DEFAULT NULL,
   `surname` varchar(64) DEFAULT NULL,
@@ -104,9 +58,9 @@ CREATE TABLE `Person` (
 # Dump of table Program
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `Program`;
+DROP TABLE IF EXISTS `program`;
 
-CREATE TABLE `Program` (
+CREATE TABLE `program` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(126) NOT NULL DEFAULT '',
   `description` text,
@@ -115,18 +69,92 @@ CREATE TABLE `Program` (
 
 
 
+# Dump of table Course
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+  `course-code` varchar(32) NOT NULL DEFAULT '',
+  `title` varchar(126) DEFAULT NULL,
+  `description` text,
+  `titular-id` varchar(12) DEFAULT NULL,
+  `department` int(11) unsigned NOT NULL,
+  `grade` int(11) DEFAULT NULL,
+  PRIMARY KEY (`course-code`),
+  KEY `titular-id` (`titular-id`),
+  KEY `department` (`department`),
+  CONSTRAINT `course_ibfk_2` FOREIGN KEY (`department`) REFERENCES `department` (`id`),
+  CONSTRAINT `course_ibfk_1` FOREIGN KEY (`titular-id`) REFERENCES `person` (`netid`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table CourseActivity
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `courseactivity`;
+
+CREATE TABLE `courseactivity` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `course-code` varchar(32) NOT NULL DEFAULT '',
+  `type` int(11) unsigned NOT NULL,
+  `semester` int(11) unsigned NOT NULL,
+  `date` int(11) DEFAULT NULL,
+  `contact-time-hours` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `course-code` (`course-code`),
+  CONSTRAINT `courseactivity_ibfk_1` FOREIGN KEY (`course-code`) REFERENCES `course` (`course-code`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table CourseEnrollment
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `courseenrollment`;
+
+CREATE TABLE `CourseEnrollment` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `course-code` varchar(32) DEFAULT NULL,
+  `netid` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `course-code` (`course-code`),
+  KEY `netid` (`netid`),
+  CONSTRAINT `courseenrollment_ibfk_2` FOREIGN KEY (`netid`) REFERENCES `person` (`netid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `courseenrollment_ibfk_1` FOREIGN KEY (`course-code`) REFERENCES `course` (`course-code`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table CourseInstructor
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `courseinstructor`;
+
+CREATE TABLE `courseinstructor` (
+  `course-activity` int(11) unsigned DEFAULT NULL,
+  `netid` varchar(12) DEFAULT NULL,
+  KEY `netid` (`netid`,`course-activity`),
+  KEY `course-activity` (`course-activity`),
+  CONSTRAINT `courseinstructor_ibfk_2` FOREIGN KEY (`netid`) REFERENCES `person` (`netid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `courseinstructor_ibfk_1` FOREIGN KEY (`course-activity`) REFERENCES `courseactivity` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
 # Dump of table ProgramChoiceCourse
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `ProgramChoiceCourse`;
+DROP TABLE IF EXISTS `programchoicecourse`;
 
-CREATE TABLE `ProgramChoiceCourse` (
+CREATE TABLE `programchoicecourse` (
   `program` int(11) unsigned NOT NULL,
   `course-code` varchar(32) NOT NULL DEFAULT '',
   KEY `program` (`program`),
   KEY `course-code` (`course-code`),
-  CONSTRAINT `programchoicecourse_ibfk_2` FOREIGN KEY (`course-code`) REFERENCES `Course` (`course-code`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `programchoicecourse_ibfk_1` FOREIGN KEY (`program`) REFERENCES `Program` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `programchoicecourse_ibfk_2` FOREIGN KEY (`course-code`) REFERENCES `course` (`course-code`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `programchoicecourse_ibfk_1` FOREIGN KEY (`program`) REFERENCES `program` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -134,43 +162,15 @@ CREATE TABLE `ProgramChoiceCourse` (
 # Dump of table ProgramMandatoryCourse
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `ProgramMandatoryCourse`;
+DROP TABLE IF EXISTS `programmandatorycourse`;
 
-CREATE TABLE `ProgramMandatoryCourse` (
+CREATE TABLE `programmandatorycourse` (
   `program` int(11) unsigned NOT NULL,
   `course-code` varchar(32) NOT NULL DEFAULT '',
   KEY `program` (`program`),
   KEY `course-code` (`course-code`),
-  CONSTRAINT `programmandatorycourse_ibfk_2` FOREIGN KEY (`course-code`) REFERENCES `Course` (`course-code`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `programmandatorycourse_ibfk_1` FOREIGN KEY (`program`) REFERENCES `Program` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table Room
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `Room`;
-
-CREATE TABLE `Room` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `capacity` int(11) unsigned NOT NULL,
-  `building` varchar(5) NOT NULL DEFAULT '',
-  `floor` int(11) unsigned NOT NULL,
-  `number` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table RoomFacility
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `RoomFacility`;
-
-CREATE TABLE `RoomFacility` (
-  `room` int(11) unsigned NOT NULL,
-  `facility` int(11) unsigned NOT NULL
+  CONSTRAINT `programmandatorycourse_ibfk_2` FOREIGN KEY (`course-code`) REFERENCES `course` (`course-code`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `programmandatorycourse_ibfk_1` FOREIGN KEY (`program`) REFERENCES `program` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -178,13 +178,13 @@ CREATE TABLE `RoomFacility` (
 # Dump of table Subscription
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `Subscription`;
+DROP TABLE IF EXISTS `subscription`;
 
-CREATE TABLE `Subscription` (
+CREATE TABLE `subscription` (
   `course-code` varchar(32) NOT NULL DEFAULT '',
   `netid` varchar(12) NOT NULL DEFAULT '',
   KEY `course-code` (`course-code`),
   KEY `netid` (`netid`),
-  CONSTRAINT `subscription_ibfk_2` FOREIGN KEY (`netid`) REFERENCES `Person` (`netid`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `subscription_ibfk_1` FOREIGN KEY (`course-code`) REFERENCES `Course` (`course-code`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `subscription_ibfk_2` FOREIGN KEY (`netid`) REFERENCES `person` (`netid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `subscription_ibfk_1` FOREIGN KEY (`course-code`) REFERENCES `course` (`course-code`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
